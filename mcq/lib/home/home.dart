20ef966f479e9c578535229c8cabed6fa03d4db2
LivingstonTardzenyuy <kongnyuylivingston2@gmail.com>
2024-01-04 08:54:57 +0100

    working on home screen to handle the state of the user

--- mcq/lib/home/home.dart
+++ mcq/lib/home/home.dart
@@ -1,21 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:mcq/services/auth.dart';
+import 'package:mcq/topics/topics.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pushNamed(context, '/about');
-          },
-          child: Text('about'
-          // style: Theme.of(context).textTheme.button.t,
-          ),
-        ),
-      ),
-    );
+    return StreamBuilder(
+        stream: AuthService().userStream,
+        builder: (context, snapshot){
+          if(snapshot.connectionState == ConnectionState.waiting){
+            return const Text('Loading');
+          } else if(snapshot.hasError){
+            return const Center(
+              child: Text("Error")
+            );
+          } else if (snapshot.hasData){
+            return const TopicsScreen();
+          }
+        });
   }
 }
