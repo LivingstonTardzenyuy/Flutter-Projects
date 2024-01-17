677811771e9431c692cca3fe43aa75f027f423a2
LivingstonTardzenyuy <kongnyuylivingston2@gmail.com>
2024-01-17 02:59:26 +0100

    working on add product admin site

--- ecommerce/lib/Pages/homePage.dart
+++ ecommerce/lib/Pages/homePage.dart
@@ -6,8 +6,24 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Home Page'),
+      appBar: AppBar(
+        title: Text('Footware Admin'),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListView.builder(
+            itemCount: 10,
+            itemBuilder: (context, index) {
+              return ListTile(
+                  title: Text('Title'),
+                  subtitle: Text('Price-200'),
+
+                trailing: IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.delete)),
+              );
+            }),
       ),
     );
   }
