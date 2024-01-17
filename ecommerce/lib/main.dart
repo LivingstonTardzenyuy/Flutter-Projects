7834fb2554cb7bb0c5cc64efc290cbc1b921f998
LivingstonTardzenyuy <kongnyuylivingston2@gmail.com>
2024-01-17 02:53:04 +0100

    connected my flutter app to firebase

--- ecommerce/lib/main.dart
+++ ecommerce/lib/main.dart
@@ -1,6 +1,13 @@
+import 'package:ecommerce/Pages/homePage.dart';
+import 'package:ecommerce/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -17,7 +24,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: ,
+      home: HomePage(),
     );
   }
 }
