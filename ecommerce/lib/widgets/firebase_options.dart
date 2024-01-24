9669c03cb0bd6cdc89e8989f52530c0d4ec27610
LivingstonTardzenyuy <kongnyuylivingston2@gmail.com>
2024-01-24 04:57:14 +0100

    succeded to generate the product.g.dart file

--- ecommerce/lib/widgets/firebase_options.dart
+++
@@ -1,3 +0,0 @@
-import 'package:firebase_core/firebase_core.dart';
-
-FirebaseOptions firebaseOptions = FirebaseOptions(apiKey: apiKey, appId: appId, messagingSenderId: messagingSenderId, projectId: projectId)
