91db8f186886e39b424c0cec893f0e0ac65ac2e8
LivingstonTardzenyuy <kongnyuylivingston2@gmail.com>
2024-01-24 09:02:22 +0100

    added logic for adding products

--- ecommerce/lib/controller/home_controller.dart
+++ ecommerce/lib/controller/home_controller.dart
@@ -1,4 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:ecommerce/model/product/product.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:get/get_state_manager/src/simple/get_controllers.dart';
 
 class HomeController extends GetxController{
@@ -14,11 +17,28 @@ class HomeController extends GetxController{
   }
 
   addProducts() {
-    DocumentReference doc = productCollection.doc(
-      {
-        'name': ''
-      }
-    );
+    try {
+      DocumentReference doc = productCollection.doc();
+      Products product = Products(
+        id: doc.id,
+        name: 'test name from flutter',
+        category: 'Boots',
+        description: 'test name from flutter',
+        price: 500,
+        brand: 'adidas',
+        image: 'image url',
+        offer: true,
+      );
+
+      //converting product to Json
+      final productJson = product.toJson();
+      doc.set(productJson);       //setting our products to document
+      Get.snackbar('Success', 'Product added successfully', colorText: Colors.green);
+
+    } catch (e){
+      Get.snackbar('Error', 'Error adding product', colorText: Colors.red,);
+      print(e);
+    }
   }
 
   testMethod() {
