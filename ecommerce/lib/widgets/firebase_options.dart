1017ec458ad21bd46882fadb5301b460ea99b44c
LivingstonTardzenyuy <kongnyuylivingston2@gmail.com>
2024-01-24 03:56:06 +0100

    initailize firebase

---
+++ ecommerce/lib/widgets/firebase_options.dart
@@ -0,0 +1,3 @@
+import 'package:firebase_core/firebase_core.dart';
+
+FirebaseOptions firebaseOptions = FirebaseOptions(apiKey: apiKey, appId: appId, messagingSenderId: messagingSenderId, projectId: projectId)
