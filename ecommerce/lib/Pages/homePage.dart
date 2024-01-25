5e7c41356def8a2971877d32f08ccc3e24676b5d
LivingstonTardzenyuy <kongnyuylivingston2@gmail.com>
2024-01-25 17:45:43 +0100

    done with admin site application

--- ecommerce/lib/Pages/homePage.dart
+++ ecommerce/lib/Pages/homePage.dart
@@ -32,7 +32,7 @@ class HomePage extends StatelessWidget {
                       },
                       icon: IconButton(
                           onPressed: () {
-                            ctrl.deleteProducts;
+                            ctrl.deleteProducts(ctrl.product[index].id ?? '');
                           },
                           icon: Icon(Icons.delete)),
                     ),
