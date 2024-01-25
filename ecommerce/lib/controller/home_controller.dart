5856c4c4ecb91726f376a4506089dbd3fd7849e1
LivingstonTardzenyuy <kongnyuylivingston2@gmail.com>
2024-01-25 17:19:58 +0100

    done fetchin

--- ecommerce/lib/controller/home_controller.dart
+++ ecommerce/lib/controller/home_controller.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_state_manager/src/simple/get_controllers.dart';
 
-class HomeController extends GetxController{
+class HomeController extends GetxController {
 
   FirebaseFirestore firestore = FirebaseFirestore.instance;
   late CollectionReference productCollection;
@@ -24,6 +24,7 @@ class HomeController extends GetxController{
   void onInit() {
     // TODO: implement onInit
     productCollection = firestore.collection('Products');
+    fetchProducts();
     super.onInit();
   }
 
@@ -43,10 +44,11 @@ class HomeController extends GetxController{
 
       //converting product to Json
       final productJson = product.toJson();
-      doc.set(productJson);       //setting our products to document
-      Get.snackbar('Success', 'Product added successfully', colorText: Colors.green);
+      doc.set(productJson); //setting our products to document
+      Get.snackbar(
+          'Success', 'Product added successfully', colorText: Colors.green);
       setValuesDefault();
-    } catch (e){
+    } catch (e) {
       Get.snackbar('Error', 'Error adding product', colorText: Colors.red,);
       print(e);
     }
@@ -56,7 +58,7 @@ class HomeController extends GetxController{
     print('test  value');
   }
 
-  setValuesDefault(){
+  setValuesDefault() {
     productNameController.clear();
     productDescriptionController.clear();
     productPriceController.clear();
@@ -70,10 +72,17 @@ class HomeController extends GetxController{
   }
 
   fetchProducts() async {
-    QuerySnapshot productSnapshot = await productCollection.get();
-    final List<Products> retriveProducts = productSnapshot.docs
+    try {
+      QuerySnapshot productSnapshot = await productCollection.get();
+      final List<Products> retriveProducts = productSnapshot.docs.map((doc) =>
+          Products.fromJson(doc.data() as Map<String, dynamic>)).toList();
+      product.clear();
+      product.assignAll(retriveProducts);
+      Get.snackbar('Success', 'Products fetch successfully', colorText: Colors.green,);
+    }
+    catch (e) {
+      Get.snackbar('Error', e.toString(), colorText: Colors.red,);
+      print(e);
+    }
   }
-
-
-
 }
