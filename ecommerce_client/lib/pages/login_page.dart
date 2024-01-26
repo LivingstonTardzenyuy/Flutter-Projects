9f66c3dd76523c1a6e582bf2c8c82181e6a1ab33
LivingstonTardzenyuy <kongnyuylivingston2@gmail.com>
2024-01-26 04:00:25 +0100

    working on ui of login page

--- ecommerce_client/lib/pages/login_page.dart
+++ ecommerce_client/lib/pages/login_page.dart
@@ -6,7 +6,38 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(),
+      body: Container(
+        padding: const EdgeInsets.all(15),
+        width: double.maxFinite,
+        decoration: BoxDecoration(
+          color: Colors.blueGrey[50]
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          // crossAxisAlignment: Cro,
+          children: [
+            Text('Welcome Back!',
+            style: TextStyle(
+              fontSize: 28,
+              fontWeight: FontWeight.w500,
+              color: Colors.deepPurple
+            ),),
+
+            SizedBox(height: 20,),
+            TextField(
+              keyboardType: TextInputType.phone,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                prefixIcon: Icon(Icons.phone_android),
+                labelText: 'Mobile Number',
+                hintText: 'Enter your mobile number'
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
