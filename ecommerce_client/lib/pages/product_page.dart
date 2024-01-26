ed13542d3ebbfe58a085b2bf06e664a6fd570c09
LivingstonTardzenyuy <kongnyuylivingston2@gmail.com>
2024-01-26 04:18:05 +0100

    wokrin

--- ecommerce_client/lib/pages/product_page.dart
+++ ecommerce_client/lib/pages/product_page.dart
@@ -6,7 +6,27 @@ class ProductPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Footware Store',
+          style: TextStyle(fontWeight: FontWeight.bold),),
+        centerTitle: true,
+        actions: [
+          IconButton(
+              onPressed: () {}, icon: Icon(Icons.logout))
+        ],
+      ),
 
+      body: Column(
+        children: [
+          SizedBox(
+            height: 50,
+            child: ListView.builder(
+                itemCount: 5,
+                itemBuilder: (context, index){
+                return Text('Category');
+          }),)
+        ],
+      ),
     );
   }
 }
