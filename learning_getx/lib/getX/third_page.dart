c274e38bab75ca36ecd56e1477141c5a74d169b3
LivingstonTardzenyuy <kongnyuylivingston2@gmail.com>
2024-02-03 14:03:02 +0100

    working on third page

--- learning_getx/lib/getX/third_page.dart
+++ learning_getx/lib/getX/third_page.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
+import 'package:learning_getx/controller/tap_controller.dart';
 
 class ThirdPage extends StatelessWidget {
   const ThirdPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    TapController controller = Get.find();
     return Scaffold(
       appBar: AppBar(
-        title: Text('Second Page'),
+        title: Text('Third Page'),
         leading: IconButton(
           onPressed: () {
             Get.back();
@@ -17,29 +20,35 @@ class ThirdPage extends StatelessWidget {
         ),
       ),
 
-      body: GestureDetector(
-        onTap: () {
+      body: GetBuilder<TapController>(
+        builder: (controller) {
+          return GestureDetector(
+            onTap: () {
+              // print('you press me');
+              controller.decrease();
+            },
+            child: Container(
+              width: double.infinity,
+              height: double.infinity,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 20),
+                    height: 100,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Color(0xFF89dad0),
+                    ),
+                    child: Center(child: Text(controller.x.toString(), style: TextStyle(color: Colors.white, fontSize: 25),)),
 
+                  )
+                ],
+              ),
+            ),
+          );
         },
-        child: Container(
-          width: double.infinity,
-          height: double.infinity,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                margin: const EdgeInsets.symmetric(horizontal: 20),
-                height: 100,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  color: Color(0xFF89dad0),
-                ),
-                child: Center(child: Text('Welcome my friend', style: TextStyle(color: Colors.white, fontSize: 25),)),
 
-              )
-            ],
-          ),
-        ),
       ),
     );
   }
