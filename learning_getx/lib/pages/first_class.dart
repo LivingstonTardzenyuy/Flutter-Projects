a7e491154d897c55d89ec06120c612614d3589ae
LivingstonTardzenyuy <kongnyuylivingston2@gmail.com>
2024-02-04 12:10:00 +0100

    adding another function

--- learning_getx/lib/pages/first_class.dart
+++ learning_getx/lib/pages/first_class.dart
@@ -1,16 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_instance/get_instance.dart';
+import 'package:get/get_navigation/get_navigation.dart';
+import 'package:learning_getx/controllers/tap_controller.dart';
 
 class FirstPage extends StatelessWidget {
   const FirstPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+
+    TapController controller = Get.find();
     return Scaffold(
       appBar: AppBar(
         title: Text('First Class'),
         leading: IconButton(
-          onPressed: () {},
-          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Get.back();
+          },
+          icon: Icon(Icons.arrow_back, color: Colors.black,),
         ),
       ),
       body: Container(
@@ -27,8 +35,25 @@ class FirstPage extends StatelessWidget {
                   borderRadius: BorderRadius.circular(20),
                   color: Color(0xFF89dad0)
               ),
-              child: Center(child: Text('tap', style: TextStyle(fontSize: 20, color: Colors.white),),),
+              child: Center(child: Text(controller.x.toString(), style: TextStyle(fontSize: 20, color: Colors.white),),),
             ),
+
+            SizedBox(height: 40,),
+            GestureDetector(
+              onTap: () {
+
+              },
+              child: Container(
+                margin: const EdgeInsets.symmetric(horizontal: 20),
+                width: double.maxFinite,
+                height: 100,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Color(0xFF89dad0)
+                ),
+                child: Center(child: Text(controller.x.toString(), style: TextStyle(fontSize: 20, color: Colors.white),),),
+              ),
+            )
           ],
         ),
       )
