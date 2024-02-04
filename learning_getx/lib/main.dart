bbe57534e1943151d6bbc90a348e6205b7918dc3
LivingstonTardzenyuy <kongnyuylivingston2@gmail.com>
2024-02-04 10:54:18 +0100

    studying getX state managment

--- learning_getx/lib/main.dart
+++ learning_getx/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
-
-import 'getX/home.dart';
+import 'package:learning_getx/pages/homePage.dart';
 
 void main() {
   runApp(const MyApp());
