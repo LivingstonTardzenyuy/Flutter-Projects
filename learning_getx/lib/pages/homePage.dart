afbd57a4b0c6b869fae376745c0be239462e5088
LivingstonTardzenyuy <kongnyuylivingston2@gmail.com>
2024-02-04 11:28:38 +0100

    ALLOWED THE UPDATE TO DISPLAY IN UI

--- learning_getx/lib/pages/homePage.dart
+++ learning_getx/lib/pages/homePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_instance/get_instance.dart';
 import 'package:learning_getx/controllers/tap_controller.dart';
@@ -18,36 +19,46 @@ class HomePage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            GestureDetector(
-              onTap: () {
-                controller.increaseX();
-              },
-              child: Container(
-                width: double.maxFinite,
-                height: 100,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: Color(0xFF89dad0)
-                ),
-                child: Center(child: Text('me', style: TextStyle(fontSize: 20, color: Colors.white),),),
 
-              ),
+            GetBuilder<TapController>(
+              builder: (controller) {
+                return GestureDetector(
+                  onTap: () {
+                    controller.increaseX();
+                  },
+                  child: Container(
+                    width: double.maxFinite,
+                    height: 100,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Color(0xFF89dad0)
+                    ),
+                    child: Center(child: Text(controller.x.toString(), style: TextStyle(fontSize: 20, color: Colors.white),),),
+
+                  ),
+                );
+              },
+
             ),
             SizedBox(height: 20,),
-            GestureDetector(
-              onTap: () {
-                print('click me');
-              },
-              child: Container(
-                width: double.maxFinite,
-                height: 100,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: Color(0xFF89dad0)
-                ),
-                child: Center(child: Text('tap', style: TextStyle(fontSize: 20, color: Colors.white),),),
+            GetBuilder<TapController>(
+              builder: (controller) {
+                return GestureDetector(
+                  onTap: () {
+                    controller.increaseX();
+                  },
+                  child: Container(
+                    width: double.maxFinite,
+                    height: 100,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Color(0xFF89dad0)
+                    ),
+                    child: Center(child: Text('tap', style: TextStyle(fontSize: 20, color: Colors.white),),),
 
-              ),
+                  ),
+                );
+              },
             ),
             SizedBox(height: 20,),
             GestureDetector(
