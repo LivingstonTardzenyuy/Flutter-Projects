4b73402d90041e04a2dfc7ffb1a72b46f741eadc
LivingstonTardzenyuy <kongnyuylivingston2@gmail.com>
2024-02-04 11:04:12 +0100

    created tap_controller to use getX

--- learning_getx/lib/pages/homePage.dart
+++ learning_getx/lib/pages/homePage.dart
@@ -6,7 +6,77 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ,
-    );
+      body: Container(
+        margin: const EdgeInsets.all(20),
+        width: double.maxFinite,
+        height: double.maxFinite,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            GestureDetector(
+              onTap: () {
+                print('click me');
+              },
+              child: Container(
+                width: double.maxFinite,
+                height: 100,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Color(0xFF89dad0)
+                ),
+                child: Center(child: Text('tap', style: TextStyle(fontSize: 20, color: Colors.white),),),
+
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                print('click me');
+              },
+              child: Container(
+                width: double.maxFinite,
+                height: 100,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Color(0xFF89dad0)
+                ),
+                child: Center(child: Text('tap', style: TextStyle(fontSize: 20, color: Colors.white),),),
+
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                print('click me');
+              },
+              child: Container(
+                width: double.maxFinite,
+                height: 100,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Color(0xFF89dad0)
+                ),
+                child: Center(child: Text('tap', style: TextStyle(fontSize: 20, color: Colors.white),),),
+
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                print('click me');
+              },
+              child: Container(
+                width: double.maxFinite,
+                height: 100,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Color(0xFF89dad0)
+                ),
+                child: Center(child: Text('tap', style: TextStyle(fontSize: 20, color: Colors.white),),),
+
+              ),
+            ),
+
+          ],
+            )
+        ),
+      );
   }
 }
