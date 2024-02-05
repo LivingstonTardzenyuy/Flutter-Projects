39dfaad34040f385bba73ea128a62524906e9315
LivingstonTardzenyuy <kongnyuylivingston2@gmail.com>
2024-02-05 09:56:56 +0100

    trying to read pdf

---
+++ learning_getx/lib/pages/pdf.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_pdfview/flutter_pdfview.dart';
+
+class PdfReader extends StatelessWidget {
+  const PdfReader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PDFView(
+        filePath: 'assets/2021.pdf',
+        // onPageChanged: (int page, int total) {
+        //   print('Page $page of $total');
+        // },
+        onViewCreated: (PDFViewController controller) {
+          // Do something with the controller if needed.
+        },
+      ),
+    )
+  }
+}
--- learning_getx/lib/pages/third_page.dart
+++ learning_getx/lib/pages/third_page.dart
@@ -4,6 +4,7 @@ import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_instance/get_instance.dart';
 import 'package:get/get_state_manager/src/simple/get_state.dart';
 import 'package:learning_getx/pages/homePage.dart';
+import 'package:learning_getx/pages/pdf.dart';
 
 import '../controllers/tap_controller.dart';
 
@@ -96,6 +97,26 @@ class ThirdPage extends StatelessWidget {
                   child: Center(child: Text('Total X + Y = ', style: TextStyle(fontSize: 20, color: Colors.white),),),
                 ),
               ),
+
+
+              SizedBox(height: 20,),
+              GestureDetector(
+                onTap: () {
+                  Get.to(() => PdfReader());
+                },
+                child: Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 20),
+                  width: double.maxFinite,
+                  height: 100,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Color(0xFF89dad0)
+                  ),
+                  child: Center(child: Text('Launch PDF', style: TextStyle(fontSize: 20, color: Colors.white),),),
+                ),
+              ),
+
+
             ],
           ),
         )
