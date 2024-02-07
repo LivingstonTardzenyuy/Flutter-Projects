5b2f0f8f2ff351c057e984cc70dd20a50bca1909
LivingstonTardzenyuy <kongnyuylivingston2@gmail.com>
2024-02-07 07:57:13 +0100

    dkfj"

--- footclient/lib/main.dart
+++ footclient/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:footclient/pages/register_page.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 // import 'package:ecommerce/firebase_options.dart';
 // import 'package:firebase_core/firebase_core.dart';
 
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
      colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
