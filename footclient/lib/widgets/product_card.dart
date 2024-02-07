e7a81bcf6ff698113154c7bc27a1373d00e8ca12
LivingstonTardzenyuy <kongnyuylivingston2@gmail.com>
2024-02-07 09:10:40 +0100

    designing the product page"

--- footclient/lib/widgets/product_card.dart
+++ footclient/lib/widgets/product_card.dart
@@ -7,19 +7,32 @@ class ProductCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       elevation: 2,
-      child: Column(
-        children: [
-          Image.network(
-            'https://indian-retailer.s3.ap-south-1.amazonaws.com/s3fs-public/2020-04/shoee.jpg',
-            fit: BoxFit.cover,
-            width: double.maxFinite,
-            height: 120,
-          ),
-          SizedBox(height: 10,),
-          Text('Price', style: TextStyle(fontSize: 16, overflow: TextOverflow.ellipsis),),
-          SizedBox(height: 10,),
-          Text('Name', style: TextStyle(fontSize: 16, overflow: TextOverflow.ellipsis),),
-        ],
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.network(
+              'https://indian-retailer.s3.ap-south-1.amazonaws.com/s3fs-public/2020-04/shoee.jpg',
+              fit: BoxFit.cover,
+              width: double.maxFinite,
+              height: 100,
+            ),
+            SizedBox(height: 10,),
+            Text('Price: 900', style: TextStyle(fontSize: 16, overflow: TextOverflow.ellipsis),),
+            SizedBox(height: 5,),
+            Text('Name: Baleri', style: TextStyle(fontSize: 16, overflow: TextOverflow.ellipsis),),
+            SizedBox(height: 4,),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 8),
+              decoration: BoxDecoration(
+                color: Colors.green,
+                borderRadius: BorderRadius.circular(4),
+              ),
+              child: Text('200% off'),
+            )
+          ],
+        ),
       ),
     );
   }
