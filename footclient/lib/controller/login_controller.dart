518ce1295888327640491c2f70942a8fc32d787e
LivingstonTardzenyuy <kongnyuylivingston2@gmail.com>
2024-02-08 06:59:36 +0100

    created a user collection

--- footclient/lib/controller/login_controller.dart
+++ footclient/lib/controller/login_controller.dart
@@ -1,5 +1,14 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
 
 class LoginController extends GetxController{
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+  late CollectionReference userCollection;
 
+  @override
+  void onInit() {
+    // TODO: implement onInit
+    super.onInit();
+    userCollection = firestore.collection('users');
+  }
 }
