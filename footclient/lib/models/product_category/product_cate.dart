7522917bff2d24c657304cfab5264ab8239b0066
LivingstonTardzenyuy <kongnyuylivingston2@gmail.com>
2024-02-22 13:30:41 +0100

    working on Categories

--- footclient/lib/models/product_category/product_cate.dart
+++ footclient/lib/models/product_category/product_cate.dart
@@ -4,5 +4,18 @@ part 'product_cate.g.dart';
 
 @JsonSerializable()
 class ProductCategory {
-  @JsonKey()
+  @JsonKey(name: "id")
+  String? id;
+
+  @JsonKey(name: "name")
+  String? name;
+
+  ProductCategory({
+    this.id,
+    this.name
+  });
+
+    factory ProductCategory.fromJson(Map<String, dynamic> json) => _$ProductCategoryFromJson(json);
+    Map<String, dynamic> toJson() => _$ProductCategoryToJson(this);
+
 }
