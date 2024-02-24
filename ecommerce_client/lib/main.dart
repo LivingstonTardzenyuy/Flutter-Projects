7a90db3333d15db1ef14b342e63e798f0b62c7a7
LivingstonTardzenyuy <kongnyuylivingston2@gmail.com>
2024-02-24 03:41:20 +0100

    ignore

--- ecommerce_client/lib/main.dart
+++ ecommerce_client/lib/main.dart
@@ -1,9 +1,11 @@
-import 'package:ecommerce_client/pages/login_page.dart';
-import 'package:ecommerce_client/pages/product_page.dart';
 import 'package:ecommerce_client/pages/register_page.dart';
 import 'package:flutter/material.dart';
+// import 'package:ecommerce/firebase_options.dart';
+// import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+Future<void> main()  async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.ini
   runApp(const MyApp());
 }
 
