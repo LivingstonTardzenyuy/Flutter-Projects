034671218ea26541cb304f39adb66d8478b8fd40
LivingstonTardzenyuy <kongnyuylivingston2@gmail.com>
2024-02-27 03:27:06 +0100

    creating files

--- chatgpt/lib/main.dart
+++ chatgpt/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'constant/constant.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -12,8 +14,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        scaffoldBackgroundColor:  
+        scaffoldBackgroundColor: scaffoldBackgroundColor,
+        appBarTheme: AppBarTheme(color: cardColor),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -61,6 +65,7 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
+      backgroundColor: Colors.amber,
       appBar: AppBar(
         // TRY THIS: Try changing the color here to a specific color (to
         // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
