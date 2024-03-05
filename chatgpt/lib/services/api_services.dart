2293579e762029c7eda63750fd927813f617d71f
LivingstonTardzenyuy <kongnyuylivingston2@gmail.com>
2024-03-05 03:37:50 +0100

    get my models in json

--- chatgpt/lib/services/api_services.dart
+++ chatgpt/lib/services/api_services.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:io';
 import 'package:http/http.dart' as http;
 import 'package:chatgpt/constant/api_const.dart';
 
@@ -6,12 +7,16 @@ import 'package:chatgpt/constant/api_const.dart';
 class ApiService{
   static Future<void> getModels() async {
     try {
-      var response = await http.get(Uri.parse("https://api.openai.com/v1/models"),
+      var response = await http.get(Uri.parse("$BASE_URL/modelsdfd"),
       headers: {
         "Authorization": "Bearer $API_KEY"},
       );
       Map jsonResponse = jsonDecode(response.body);           //decoding the response
-      print("jsonresponse $jsonResponse");
+      if(jsonResponse['error'] != null){
+        print("jsonResponse['error'] ${jsonResponse['error']['message']}");
+        throw HttpException(jsonResponse['error']['message']);
+      }
+      print("jsonresponse: $jsonResponse");
     } catch(error) {
       print('error $error');
     }
